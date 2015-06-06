fb414b2b2b1333bb6d9a48385b9e671b52c659a4
Thoum <indiofish@gmail.com>
2015-06-06 17:14:34 +0900

    write to file

--- ass3/Ass2.java
+++ ass3/Ass2.java
@@ -1,5 +1,5 @@
-//TODO write to a file.
 import java.util.ArrayList;
+import java.io.*;
 public class Ass2 
 {
   //TODO add error catching.
@@ -17,11 +17,9 @@ public class Ass2
     }
 
     public void push(T obj)
-    {//push the object to stack, and increase count
+    {//push the object to stack, and increase count,size will be automatically managed
       stack.add(count,obj);
       count++;
-      //unnecessary?
-      //incSize();
     }
 
     public T pop() //throws Exception 
@@ -70,6 +68,7 @@ public class Ass2
     }
 
 
+    //unnecessary
     private void incSize()
     {//if stack is full, increase size by double.
       if (count >= maxSize) {
@@ -81,31 +80,35 @@ public class Ass2
     }
 
   }
-   
+
   public static void main (String[] args) 
   {
     MyStack<Character> stk = new MyStack<>(100);
     String test="+ C + o + m + p + u + t + e - + r - + P + r + o + g - + r + a + m + 1 + 5";
+    //remove spaces
     test = test.replaceAll("\\s+","");
 
-    //TODO print this to file
-    for (int i = 0; i < test.length(); i++) {
-      if (test.charAt(i) == '+') {
-        //if +, read the next char to add
-        i++;
-        System.out.print("[push] ");
-        System.out.println(test.charAt(i));
-        stk.push(test.charAt(i));
-      } 
-      else if (test.charAt(i) == '-') {
-        System.out.print("[pop] ");
-        try {
-          System.out.println(stk.pop());
-        } catch(IndexOutOfBoundsException e) {
-          System.out.println("Illegal request of item: " + e.toString());
+    try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+            new FileOutputStream("stack.txt"), "utf-8"))) 
+    {
+      for (int i = 0; i < test.length(); i++) {
+        if (test.charAt(i) == '+') {
+          //if +, read the next char to add
+          i++;
+          writer.write("[push] "+ test.charAt(i)+"\n");
+          stk.push(test.charAt(i));
+        } 
+        else if (test.charAt(i) == '-') {
+          try {
+            writer.write("[pop] " + stk.pop()+"\n");
+          } catch(IndexOutOfBoundsException e) {
+            System.out.println("Stack out of bounds" + e.toString());
+          }
         }
       }
-    }
-    System.out.println(stk);
+      writer.write(stk.toString());
+    }catch (IOException ex) {
+      System.out.println("Unable to Write to File");
+    } 
   }
 }
